18f9c6bc2b3b8f084d09f2e5963ad5df702920c6
SG28 <sharvingupta@gmail.com>
2025-05-13 23:27:53 +0800

    part 1

--- read_serial.py
+++ read_serial.py
@@ -9,3 +9,23 @@ sample_rate = 6400
 num_of_channels = 1
 bytes_per_sample = 2
 
+# Part 1
+# Ask user to choose an operating mode
+print("=============== MENU ===============")
+print("1 -> Manual Recording Mode")
+print("2 -> Distance Trigger Mode")
+mode_choice = int(input(">> Enter mode : "))
+if (mode_choice != 1 and mode_choice != 2):
+    print("Invalid mode detected.")
+    exit()
+# Modify parameters of each operating mode
+print("======== Parameter Settings ========")
+if (mode_choice == 1):
+    audio_duration = int(input(">> Enter audio duration (s) : "))
+    operation_command = "1     "
+else:
+    ultrasonic_distance = int(input(">> Enter ultrasonic sensor threshold distance (cm) : "))
+    audio_duration = int(input(">> Enter audio duration (s) : "))
+    operation_command = f"2 - {ultrasonic_distance}"
+print("")
+
